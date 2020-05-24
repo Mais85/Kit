72d2150af19e1e75528c916d298acf4ce833b62e
Mais85 <mayis.ras9@gmail.com>
2020-05-24 16:49:05 +0400

    24 may home lang

--- app/Http/Controllers/Site/ProjectsController.php
+++ app/Http/Controllers/Site/ProjectsController.php
@@ -25,7 +25,7 @@ class ProjectsController extends HomeController
 
     public function show( $local,$slug)
     {
-//        dd(\request()->getUri());
+        dd(\request()->getUri());
         $item = $this->projectrepository->getProjectbySlug($slug);
         return view('site.project',compact('item'));
     }
